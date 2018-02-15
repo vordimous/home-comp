f800bcf8f7907dc263c24e71e39e81d164ae3e4d
vordimous <vordimous@outlook.com>
2018-02-15 10:38:25 -0500

    update icons

--- src/components/InputsItem.vue
+++ src/components/InputsItem.vue
@@ -3,7 +3,7 @@
   <v-card-title class="pa-1" primary-title v-bind:style="{'background-color': item.color}">
     <v-btn @click="checkDel" small icon class="shadow"><v-icon>delete</v-icon></v-btn>
   </v-card-title>
-   <v-container fluid class="pb-1">
+  <v-container fluid class="pb-1">
     <v-layout row>
       <v-flex xs5 class="pa-1">
         <v-text-field
@@ -11,37 +11,42 @@
           disabled
           :value="item.monPmt.toFixed(2)"
           prefix="$"
-          @change="recalc"
           hide-details
           type="number"></v-text-field>
       </v-flex>
-      <v-flex xs1 class="pa-1">
-        <v-tooltip right>
-          <v-icon color="info" slot="activator">info</v-icon>
-          <span>All expenses broken down per month:</span>
-          <v-list dense>
-            <v-list-tile>
-              <v-list-tile-content>Mortgage Payment:</v-list-tile-content>
-              <v-list-tile-content class="align-end">${{ item.morgPmt.toFixed(2) }}</v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>Property Tax:</v-list-tile-content>
-              <v-list-tile-content class="align-end">${{ item.propTax.toFixed(2) }}</v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>Homeowners Insurace:</v-list-tile-content>
-              <v-list-tile-content class="align-end">${{ item.insur.toFixed(2) }}</v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content>PMI:</v-list-tile-content>
-              <v-list-tile-content class="align-end">${{ item.pmi.toFixed(2) }}</v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile>
-              <v-list-tile-content class="pr-1">Maintenance/Improvements:</v-list-tile-content>
-              <v-list-tile-content class="align-end">${{ ((item.main + item.improv) / 12).toFixed(2) }}</v-list-tile-content>
-            </v-list-tile>
-          </v-list>
-        </v-tooltip>
+      <v-flex xs1 class="pa-0">
+        <v-container text-xs-center fill-height class="pa-0">
+          <v-layout row wrap align-center class="pa-0">
+            <v-flex xs12 class="pa-0">
+              <v-tooltip right>
+                <v-icon small slot="activator">fa-info</v-icon>
+                <span>All expenses broken down per month:</span>
+                <v-list dense>
+                  <v-list-tile>
+                    <v-list-tile-content>Mortgage Payment:</v-list-tile-content>
+                    <v-list-tile-content class="align-end">${{ item.morgPmt.toFixed(2) }}</v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content>Property Tax:</v-list-tile-content>
+                    <v-list-tile-content class="align-end">${{ item.propTax.toFixed(2) }}</v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content>Homeowners Insurace:</v-list-tile-content>
+                    <v-list-tile-content class="align-end">${{ item.insur.toFixed(2) }}</v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content>PMI:</v-list-tile-content>
+                    <v-list-tile-content class="align-end">${{ item.pmi.toFixed(2) }}</v-list-tile-content>
+                  </v-list-tile>
+                  <v-list-tile>
+                    <v-list-tile-content class="pr-1">Maintenance/Improvements:</v-list-tile-content>
+                    <v-list-tile-content class="align-end">${{ ((item.main + item.improv) / 12).toFixed(2) }}</v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-tooltip>
+            </v-flex>
+          </v-layout>
+        </v-container>
       </v-flex>
       <v-flex xs6 class="pa-1">
         <v-text-field
@@ -49,7 +54,6 @@
           disabled
           :value="item.morgPmt.toFixed(2)"
           prefix="$"
-          @change="recalc"
           hide-details
           type="number"></v-text-field>
       </v-flex>
@@ -158,6 +162,100 @@
     </v-layout>
     <!-- <pre> {{JSON.stringify(item, null, 1)}}</pre> -->
   </v-container>
+  <v-divider></v-divider>
+  <v-container fluid class="pb-0 pl-2"><h4>Totals after {{item.lenMorg}} yrs:</h4></v-container>
+  <v-container fluid class="pb-1">
+    <v-layout row>
+      <v-flex xs5 class="pa-1">
+        <v-text-field
+          label="Total Intrest"
+          disabled
+          :value="item.monPmt.toFixed(2)"
+          prefix="$"
+          hide-details
+          type="number"></v-text-field>
+      </v-flex>
+      <v-flex xs1 class="pa-0">
+        <v-container text-xs-center fill-height class="pa-0">
+          <v-layout row wrap align-center class="pa-0">
+            <v-flex xs12 class="pa-0">
+              <v-tooltip right>
+                <v-icon small slot="activator">fa-info</v-icon>
+                <span>All expenses broken down per month:</span>
+              </v-tooltip>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+      <v-flex xs5 class="pa-1">
+        <v-text-field
+          label="Total Insurance/Upkeep"
+          disabled
+          :value="item.morgPmt.toFixed(2)"
+          prefix="$"
+          hide-details
+          type="number"></v-text-field>
+      </v-flex>
+      <v-flex xs1 class="pa-0">
+        <v-container text-xs-center fill-height class="pa-0">
+          <v-layout row wrap align-center class="pa-0">
+            <v-flex xs12 class="pa-0">
+              <v-tooltip right>
+                <v-icon small slot="activator">fa-info</v-icon>
+                <span>All expenses broken down per month:</span>
+              </v-tooltip>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs5 class="pa-1">
+        <v-text-field
+          label="Spent on income"
+          disabled
+          :value="item.monPmt.toFixed(2)"
+          prefix="$"
+          @change="recalc"
+          hide-details
+          type="number"></v-text-field>
+      </v-flex>
+      <v-flex xs1 class="pa-0">
+        <v-container text-xs-center fill-height class="pa-0">
+          <v-layout row wrap align-center class="pa-0">
+            <v-flex xs12 class="pa-0">
+              <v-tooltip right>
+                <v-icon small slot="activator">fa-info</v-icon>
+                <span>All expenses broken down per month:</span>
+              </v-tooltip>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+      <v-flex xs5 class="pa-1">
+        <v-text-field
+          label="Mortgage Payment"
+          disabled
+          :value="item.morgPmt.toFixed(2)"
+          prefix="$"
+          @change="recalc"
+          hide-details
+          type="number"></v-text-field>
+      </v-flex>
+      <v-flex xs1 class="pa-0">
+        <v-container text-xs-center fill-height class="pa-0">
+          <v-layout row wrap align-center class="pa-0">
+            <v-flex xs12 class="pa-0">
+              <v-tooltip right>
+                <v-icon small slot="activator">fa-info</v-icon>
+                <span>All expenses broken down per month:</span>
+              </v-tooltip>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </v-card>
 </template>
 
@@ -185,6 +283,9 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .shadow {
-  text-shadow: 0 0 1px #303030
+  text-shadow: 0 0 1px #303030;
+}
+.tooltip--right i {
+  color: #9e9e9e !important;
 }
 </style>
