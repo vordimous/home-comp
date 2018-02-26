54c79a39c529ea8faa68c7774e9776ab9cfd15e1
vordimous <vordimous@outlook.com>
2018-02-26 14:36:18 -0500

    update title and icon

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Calc',
+      redirect: '/home',
+    },
+    {
+      path: '/home',
+      name: 'Home',
       component: Calculator,
     },
   ],
